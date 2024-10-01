39e8d889a2998ce232e8abe4fb79b461cae35131
Kumar Gourab Barik <116557339+Kumar00100@users.noreply.github.com>
2024-10-02 01:09:30 +0530

    My First JAva Programm

---
+++ MyFirstJavaProgramming.java
@@ -0,0 +1,5 @@
+    public class MyFirstJavaProgramming {
+        public static void main(String[] args) {
+            System.out.println("Hellow world thish is my first java program");
+        }
+    }
