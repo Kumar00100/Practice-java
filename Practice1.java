e2771381b1b722c3c0ab9f5f11c20ab3dd4ab35b
Kumar Gourab Barik <116557339+Kumar00100@users.noreply.github.com>
2024-10-02 02:25:39 +0530

    USing Numers

---
+++ Practice1.java
@@ -0,0 +1,15 @@
+public class Practice1 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+        int num4 = num1 + num2 + num3;
+        int num5 = num1 - num2 - num3;
+        int num6 = num1 * num2 * num3;
+        int num7 = num1 / num2 / num3;
+        System.out.println(num4);
+        System.out.println(num5);
+        System.out.println(num6);
+        System.out.println(num7);
+    }
+}
