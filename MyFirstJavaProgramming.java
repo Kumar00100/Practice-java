c4ff096a8dec502c0d4f4e89e927160264d52f17
Kumar Gourab Barik <116557339+Kumar00100@users.noreply.github.com>
2024-10-02 01:14:47 +0530

    Adding Two Numbers

--- MyFirstJavaProgramming.java
+++ MyFirstJavaProgramming.java
@@ -1,5 +1,9 @@
     public class MyFirstJavaProgramming {
         public static void main(String[] args) {
+            int a=1;
+            int b=20;
+            int c=a+b;
+            System.out.println(c);
             System.out.println("Hellow world thish is my first java program");
         }
     }
